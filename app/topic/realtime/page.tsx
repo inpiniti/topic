9ad3772c6b36ce0806e50753869458aa1d25b2dd
ykjung <ykjung@scgs.co.kr>
2025-01-03 18:52:40 +0900

    실시간 조회가 안되는 부분이 있어서 수정해봄

--- app/topic/realtime/page.tsx
+++ app/topic/realtime/page.tsx
@@ -19,6 +19,7 @@ export default async function DailyComponent() {
       headers: {
         'Content-Type': 'application/json',
       },
+      cache: 'no-cache',
     }
   );
   const topics = await response.json();
